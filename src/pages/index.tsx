6db364f4d331cba6f031bcd5f024f14936843f90
liptpb <liptpb@163.com>
2022-01-03 20:46:52 +0800

    1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,107 +1,61 @@
-// import styles from './index.less';
-// import React, { Component } from 'react';
-// import ReactToPrint from 'react-to-print';
-import Barcode from "./components/Barcode/index";
 
-// export default function IndexPage() {
-//   const componentRef = React.useRef(null);
-//   const reactToPrintContent = React.useCallback(() => {
-//     return componentRef.current;
-//   }, [componentRef.current]);
-//   return (
-//     <div>
-//       <div  ref={componentRef}>
-//     <div style={{textAlign: 'center',color: 'red',fontSize: '40px',marginBottom: '10px',marginTop: '40px'}}>活动二维码</div>
-//     <div style={{fontSize: '26px',textAlign: 'center'}}>活动标题</div>
-//     <Barcode value="12345678" />
-// </div>
-// <button style={{ marginRight: 16,marginLeft: 30 }}  onClick={() => this.handleCancel()}>取消</button>
-
-// <ReactToPrint
-//     trigger={() => <a ><button >打印二维码</button></a>}
-//     content={reactToPrintContent}
-//     onAfterPrint={()=>{
-//      console.log('打印成功！');
-//     }}
-//     onPrintError={(errorLocation: string, error: Error)=>{
-//       console.log(errorLocation);
-//       console.log(error);
-//      }}
-// />
-//       <h1 className={styles.title}>Page index</h1>
-     
-//     </div>
-//   );
-// }
 import * as React from "react";
 
-import { ComponentToPrint } from "./demo";
-import { useReactToPrint } from 'react-to-print';
+// import { ComponentToPrint } from "./demo";
+// import { useReactToPrint } from 'react-to-print';
 
-export default function FunctionalComponentWithHook() {
-  const componentRef = React.useRef(null);
-  const onBeforeGetContentResolve = React.useRef<(() => void) | null>(null);
+// export default function FunctionalComponentWithHook() {
+//   // FileSystemObject可以随便修改用户电脑中的文件，在客户端使用非常不安全，一般客户端浏览器都是禁止使用的。 只有在hta网页应用程序中或者在服务器端才允许使用
+//   const handlePrint = () =>{
+//     console.log('1111')
+//     var fso = new ActiveXObject("Scripting.FileSystemObject");
+//     // var f1 = fso.createtextfile("E:\\myjstest.txt",true)
+//     var xmlHttp
+//       //判断浏览器是否支持ActiveX控件
+//       if(window.ActiveXObject){
+//         console.log('yse')
+//         //支持-通过ActiveXObject的一个新实例来创建XMLHttpRequest对象
+//         console.log('yse')
+//         xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+//       }
+//       //不支持
+//       else if(window.XMLHttpRequest){
+//         console.log('no')
+//         xmlHttp = new XMLHttpRequest()
+//       }
+//   }
 
-  const [loading, setLoading] = React.useState(false);
-  const [text, setText] = React.useState("old boring text");
 
-  const handleAfterPrint = React.useCallback(() => {
-    console.log("`onAfterPrint` called"); // tslint:disable-line no-console
-  }, []);
 
-  const handleBeforePrint = React.useCallback(() => {
-    console.log("`onBeforePrint` called"); // tslint:disable-line no-console
-  }, []);
-
-  const handleOnBeforeGetContent = React.useCallback(() => {
-    console.log("`onBeforeGetContent` called"); // tslint:disable-line no-console
-    setLoading(true);
-    setText("Loading new text...");
-
-    return new Promise<void>((resolve) => {
-      onBeforeGetContentResolve.current = resolve;
-
-      setTimeout(() => {
-        setLoading(false);
-        setText("New, Updated Text!");
-        resolve();
-      }, 2000);
-    });
-  }, [setLoading, setText]);
-
-  const reactToPrintContent = React.useCallback(() => {
-    return componentRef.current;
-  }, [componentRef.current]);
-
-  const handlePrint = useReactToPrint({
-    content: reactToPrintContent,
-    documentTitle: "AwesomeFileName",
-    onBeforeGetContent: handleOnBeforeGetContent,
-    onBeforePrint: handleBeforePrint,
-    onAfterPrint: handleAfterPrint,
-    removeAfterPrint: false,
-  });
-
-  React.useEffect(() => {
-    if (text === "New, Updated Text!" && typeof onBeforeGetContentResolve.current === "function") {
-      onBeforeGetContentResolve.current();
-    }
-  }, [onBeforeGetContentResolve.current, text]);
-
-  return (
+//   return (
+//     <div>
+//       <button onClick={handlePrint}>
+//         Print
+//       </button>
+//     </div>
+//   );
+// };
+interface Props {
+  
+}
+ 
+const aaa = () => {
+  const handleSubmit = () => {
+    console.log(this.refs.input.value)   //方法一
+  
+  }
+  return ( 
     <div>
-      {loading && <p className="indicator">onBeforeGetContent: Loading...</p>}
-      <button onClick={handlePrint}>
-        Print
-      </button>
-      {/* <div ref={componentRef} >1111</div> */}
-      <div style={{'display':'none'}} >
-           <div ref={componentRef}>
-              <div style={{'position':'absolute',top:20,right:30}} >
-                 <Barcode value="12345678" />
-              </div>
-           </div>
-      </div>
+        <input
+                    type='text'
+                    ref='input' defaultValue="Hello" />
+                    <button type='submit' onClick={handleSubmit}>Submit</button>
+                //方法二
+                 <input
+                    type='text'
+                    ref={(val) => this.inputdata = val} defaultValue="Hello" />
     </div>
-  );
-};
+   );
+}
+ 
+export default aaa;
